9db05360b42bd51956e4d0a93822e69d2e0438a2
mahfuj-bd <mahfujshakibbd@gmai.com>
2023-12-02 01:56:49 +0600

    added all jobs

--- src/Pages/AllJObs/AllJObs.jsx
+++ src/Pages/AllJObs/AllJObs.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const AllJObs = () => {
     const allJobs = useLoaderData();
@@ -19,132 +19,56 @@ const AllJObs = () => {
                       />
                     </label>
                   </th>
-                  <th className="text-main-blue-50">Author</th>
-                  <th className="text-main-blue-50">Title</th>
-                  <th className="text-main-blue-50">Posted Date</th>
-                  <th className="text-main-blue-50">Publisher</th>
-                  <th className="text-main-blue-50">isPremium</th>
-                  <th className="text-main-blue-50">Status</th>
-  
-                  <th className="text-main-blue-50">Decline</th>
+                  <th className="text-main-blue-50">Name who posted the job</th>
+                  <th className="text-main-blue-50">Job Title</th>
+                  <th className="text-main-blue-50">Job Posting Date</th>
+                  <th className="text-main-blue-50">Application Deadline</th>
+                  <th className="text-main-blue-50">Salary range</th>
+                  <th className="text-main-blue-50">Details Button</th>                
                 </tr>
               </thead>
               <tbody>
                 {/* row 1 */}
-                {((article) => (
-                  <tr key={article?._id}>
-                    <th>
-                      <button
-                        onClick={() => (article?._id)}
-                        size={23}
-                        className="text-[#ff3131] cursor-pointer"
-                      />
-                      <button/>
-                    </th>
+                {allJobs.map((job) => (
+                  <tr key={job?._id}>
+                    
                     <td>
                       <div className="flex items-center gap-3">
                         <div className="avatar">
                           <div className="mask mask-squircle w-12 h-12">
                             <img
-                              src={article?.author_photoURL}
+                              src={job?.jobBanner
+                              }
                               alt="Avatar Tailwind CSS Component"
                             />
                           </div>
                         </div>
-  
+                              
                         <div>
                           <div className="font-bold text-main-blue-50">
-                            {article?.author}
+                            
                           </div>
                           <div className="text-sm opacity-70">
-                            {article?.author_email}
+                           
                           </div>
                         </div>
                       </div>
                     </td>
-                    <td>
-                      {article?.title}
-                      <br />
-                    </td>
-                    <td>{article.posting_date}</td>
-                    <td>{article?.publisher}</td>
-                    <td>
-                      {article?.premium ? (
-                        <h1 className="font-bold text-violet-800">Premium</h1>
-                      ) : (
-                        <button
-                          
-                          className="w-20 font-bold py-3 rounded-lg text-main-blue-50 bg-gradient-to-r from-[#6ba5ef] to-[#3367dd]"
-                        >
-                          Make Premium
-                        </button>
-                      )}
-                    </td>
-                    <td>
-                      {article?.status === "approved" ? (
-                        <h1 className="font-bold text-green-800"> Approved</h1>
-                      ) : (
-                        <button
-                          
-                          className="w-20 font-bold py-3 rounded-lg text-main-blue-50 bg-gradient-to-r from-green-100 to-green-600"
-                        >
-                          Approve Article
-                        </button>
-                      )}
-                    </td>
-                    <th>
-                      {article.status === "approved" ||
-                      article.status === "pending" ? (
-                        <div>
-                          {" "}
-                          <button
-                            className=" w-20 font-bold py-3 rounded-lg text-main-blue-50 bg-gradient-to-r from-[#e75050] to-[#dd3333]"
-                            onClick={() =>
-                              document.getElementById(article?._id).showModal()
-                            }
-                          >
-                            Declined
-                          </button>
-                          <dialog id={article?._id} className="modal">
-                            <div className="modal-box">
-                              <h3 className="font-bold text-main-blue-950 text-lg">
-                                Reason for Decline
-                              </h3>
-  
-                              <input
-                                type="text"
-                                name="reason"
-                                placeholder="Reason...."
-                               
-                                className="input mt-4 input-bordered border-2 text-main-blue-950 border-main-blue-300 rounded-lg w-full "
-                              />
-                              <button
-                                
-                                className=" px-6 mt-5 font-bold py-3 rounded-lg text-main-blue-50 bg-gradient-to-r from-[#e75050] to-[#dd3333]"
-                              >
-                                Send
-                              </button>
-  
-                              <p className="py-4 text-main-blue-950">
-                                Press ESC key or click outside to close
-                              </p>
-                            </div>
-                            <form method="dialog" className="modal-backdrop">
-                              <button>close</button>
-                            </form>
-                          </dialog>
-                        </div>
-                      ) : (
-                        <h1 className="text-red-600">Declined</h1>
-                      )}
-                    </th>
+                    <td>{job?.postedBy}</td>
+                    <td>{job?.jobTitle}</td>
+                    <td>{job?.postingDate}</td>
+                    <td>{job?.applicationDeadline}</td>
+                    <td>{job?.salaryRange}</td>
+                    <td><button className="btn"><Link to={`/jobdetails/${job._id}`}>Details</Link></button></td>                                   
                   </tr>
                 ))}
               </tbody>
             </table>
-          </div>        
+          </div>
+         
         </div>
       </div>
+
     );
 };
 
